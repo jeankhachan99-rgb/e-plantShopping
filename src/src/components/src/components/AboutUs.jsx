5233be37e50ba10e9c1a8129a3199e470d1546c9
jeankhachan99-rgb <jeankhachan99@gmail.com>
2025-12-14 14:24:05 +0100

    Move AboutUs.jsx to correct path: src/components/AboutUs.jsx

--- src/src/components/src/components/AboutUs.jsx
+++ src/src/components/src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ function AboutUs() {
     <div>
       <h2>About Paradise Nursery</h2>
       <p>
-        Paradise Nursery offers a wide range of indoor and outdoor plants to enhance your living space. We are committed to helping you grow a greener world.
+        Paradise Nursery offers a variety of plants to enhance your indoor and outdoor spaces. We are passionate about greenery!
       </p>
     </div>
   );
