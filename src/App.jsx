0b043daf4a2e9f4aa770560d1d11986a2c6348ad
jeankhachan99-rgb <jeankhachan99@gmail.com>
2025-12-14 14:38:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AboutUs from "./components/AboutUs";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
+  const handleGetStarted = () => {
+    setShowProducts(true);
+  };
+
   return (
     <div className="App">
       <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+      <button onClick={handleGetStarted}>Get Started</button>
+
+      {showProducts && (
+        <div>
+          <h2>Featured Plants</h2>
+          <ul>
+            <li>Fiddle Leaf Fig</li>
+            <li>Snake Plant</li>
+            <li>Peace Lily</li>
+          </ul>
+        </div>
+      )}
+
       <AboutUs />
     </div>
   );
 }
 
 export default App;
+
